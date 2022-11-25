8e453e12a392ba04a105b4b4f6fc13dd487193cd
jhun1020 <jhun1020.kh@gmail.com>
2022-11-25 12:08:53 +0900

    dsdda

--- docs/browser_js/js/javascriptWithPoll_us2.js
+++ docs/browser_js/js/javascriptWithPoll_us2.js
@@ -161,9 +161,9 @@ function setPollContent() {
     )}</div><br>`;
     polls[index]["answer_uids"].forEach((answer_uid, index) => {
       // console.log(`${index + 1}. ${getAnswerByUid(answer_uid)}`);
-      desc += `<div><input type="radio" name="choice">${
+      desc += `<div><input type="radio" name="choice" id="radio${index}"><label for="radio${index}">${
         index + 1
-      }. ${getAnswerByUid(answer_uid)}</div>`;
+      }. ${getAnswerByUid(answer_uid)}</label></div>`;
     });
     queryContent.innerHTML = desc;
     index++;
@@ -192,9 +192,9 @@ function setPollContent_prev() {
     // 답변부분
     polls[index - 2]["answer_uids"].forEach((answer_uid, index) => {
       // console.log(`${index + 1}. ${getAnswerByUid(answer_uid)}`);
-      desc_prev += `<div><input type="radio" name="choice"> ${
+      desc_prev += `<div><input type="radio" name="choice" id="radio${index}"><label for="radio${index}"> ${
         index + 1
-      }. ${getAnswerByUid(answer_uid)}</div>`;
+      }. ${getAnswerByUid(answer_uid)}</label></div>`;
     });
 
     queryContent_prev.innerHTML = desc_prev;
